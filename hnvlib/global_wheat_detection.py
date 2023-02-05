61e127f2af0604aa3ead29f2948c4219b03b8abb
leeyeoreum02 <leeyeoreum02@gmail.com>
2023-02-06 03:17:07 +0900

    2/6 fix global_wheat_detection

--- hnvlib/global_wheat_detection.py
+++ hnvlib/global_wheat_detection.py
@@ -5,9 +5,12 @@ Wheat Dataset Link : https://www.kaggle.com/c/global-wheat-detection
 from collections import defaultdict
 import os
 import shutil
-from typing import Callable, Optional, Sequence, Tuple
+from typing import Callable, Optional, Sequence, Tuple, TypeVar
 import random
 import re
+from pprint import pprint
+import json
+from ast import literal_eval
 
 import pandas as pd
 from PIL import Image
@@ -15,16 +18,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.patheffects as pe
+from pycocotools.coco import COCO
+from pycocotools.cocoeval import COCOeval
 
 import torch
+import torchvision
 from torch import Tensor, nn
 from torch.utils.data import Dataset, DataLoader
 from torch import optim
 from torchvision.transforms import ToTensor, Compose
-from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn, FastRCNNPredictor, FasterRCNN_ResNet50_FPN_Weights
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
-from pytorch_lightning import LightningModule
+from pytorch_lightning import LightningModule, Trainer
+from pytorch_lightning.loggers import WandbLogger
+
+
+_device = TypeVar('_device')
+
+
+torch.set_float32_matmul_precision('medium')
+torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+def split_dataset(csv_path: os.PathLike, split_rate: float = 0.2) -> None:
+    """Dirty-MNIST 데이터셋을 비율에 맞춰 train / test로 나눕니다.
+    
+    :param path: Dirty-MNIST 데이터셋 경로
+    :type path: os.PathLike
+    :param split_rate: train과 test로 데이터 나누는 비율
+    :type split_rate: float
+    """
+    df = pd.read_csv(csv_path)
+    df = df.sample(frac=1).reset_index(drop=True)
+
+    grouped = df.groupby(by='image_id')
+    grouped_list = [group for _, group in grouped]
+
+    split_point = int(split_rate * len(grouped))
+
+    save_dir = os.path.split(csv_path)[0]
+    test_df = pd.concat(grouped_list[:split_point])
+    test_df.to_csv(os.path.join(save_dir, 'test_answer.csv'), index=False)
+    train_df = pd.concat(grouped_list[split_point:])
+    train_df.to_csv(os.path.join(save_dir, 'train_answer.csv'), index=False)
 
 
 class WheatDataset(Dataset):
@@ -35,7 +71,6 @@ class WheatDataset(Dataset):
         image_dir: os.PathLike,
         csv_path: os.PathLike,
         transform: Optional[Sequence[Callable]] = None,
-        is_test: bool = False
     ) -> None:
         """데이터 정보를 불러와 정답(bbox)과 각각 데이터의 이름(image_id)를 저장
         
@@ -49,20 +84,18 @@ class WheatDataset(Dataset):
         :type is_test: bool
         """
         super().__init__()
+
         df = pd.read_csv(csv_path)
         self.image_dir = image_dir
         self.image_ids = df['image_id'].unique()
-
-        self.is_test = is_test
-
-        if not self.is_test:
-            self.all_bboxes = defaultdict(list)
-            for _, row in df.iterrows():
-                bbox = re.sub('\[|\]', '', row['bbox'])
-                bbox = list(map(float, bbox.split(', ')))
-                x, y, w, h = bbox
-                bbox = [x, y, x + w, y + h]
-                self.all_bboxes[row['image_id']].append(bbox)
+        
+        self.all_bboxes = defaultdict(list)
+        for _, row in df.iterrows():
+            bbox = re.sub('\[|\]', '', row['bbox'])
+            bbox = list(map(float, bbox.split(', ')))
+            x, y, w, h = bbox
+            bbox = [x, y, x + w, y + h]
+            self.all_bboxes[row['image_id']].append(bbox)
 
         self.transform = transform
 
@@ -89,9 +122,6 @@ class WheatDataset(Dataset):
         if self.transform is not None:
             image = self.transform(image)
 
-        if self.is_test:
-            return image
-
         bboxes_per_image = self.all_bboxes[image_id]
         class_labels = [1] * len(bboxes_per_image)
 
@@ -100,7 +130,11 @@ class WheatDataset(Dataset):
             'labels': torch.as_tensor(class_labels, dtype=torch.int64)
         }
 
-        return image, target
+        return image, target, image_id
+    
+
+def collate_fn(batch: Tensor) -> Tuple:
+    return tuple(zip(*batch))
 
 
 def visualize_dataset(image_dir: os.PathLike, csv_path: os.PathLike, save_dir: os.PathLike, n_images: int = 10) -> None:
@@ -122,9 +156,8 @@ def visualize_dataset(image_dir: os.PathLike, csv_path: os.PathLike, save_dir: o
 
     indices = random.Random(36).choices(range(len(dataset)), k=n_images)
     for i in indices:
-        image, target = dataset[i]
+        image, target, image_id = dataset[i]
         image = np.array(image)
-        image_id = dataset.image_ids[i]
 
         plt.imshow(image)
         ax = plt.gca()
@@ -164,10 +197,6 @@ def visualize_dataset(image_dir: os.PathLike, csv_path: os.PathLike, save_dir: o
         plt.clf()
 
 
-def collate_fn(batch: Tensor) -> Tuple:
-    return tuple(zip(*batch))
-
-
 def train(dataloader: DataLoader, device: str, model: nn.Module, optimizer: torch.optim.Optimizer) -> None:
     """Wheat 데이터셋으로 뉴럴 네트워크를 훈련합니다.
     
@@ -182,23 +211,160 @@ def train(dataloader: DataLoader, device: str, model: nn.Module, optimizer: torc
     """
     size = len(dataloader.dataset)
     model.train()
-    for batch, (images, targets) in enumerate(dataloader):
+    for batch, (images, targets, _) in enumerate(dataloader):
         images = [image.to(device) for image in images]
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
         loss_dict = model(images, targets)
-        losses = sum(loss for loss in loss_dict.values())
+        loss = sum(loss for loss in loss_dict.values())
 
         optimizer.zero_grad()
-        losses.backward()
+        loss.backward()
         optimizer.step()
 
-        if batch % 100 == 0:
+        if batch % 10 == 0:
             current = batch * len(images)
-            print(f'total loss: {losses:>4f}, cls loss: {loss_dict["loss_classifier"]:>4f}, box loss: {loss_dict["loss_box_reg"]:>4f} [{current:>5d}/{size:>5d}]')
+            print(f'total loss: {loss:>4f}, cls loss: {loss_dict["loss_classifier"]:>4f}, box loss: {loss_dict["loss_box_reg"]:>4f}, obj loss: {loss_dict["loss_objectness"]:>4f}, rpn loss: {loss_dict["loss_rpn_box_reg"]:>4f} [{current:>5d}/{size:>5d}]')
 
 
-def visualize_predictions(testset: Dataset, device: str, model: nn.Module, save_dir: os.PathLike, conf_thr: float = 0.1) -> None:
+class MeanAveragePrecision:
+    def __init__(self, csv_path: os.PathLike) -> None:
+        self.id_csv2coco = {}
+        json_path = self.to_coco(csv_path)
+        self.coco_gt = COCO(json_path)
+
+        self.preds = []
+
+    def to_coco(self, csv_path: os.PathLike) -> os.PathLike:
+        df = pd.read_csv(csv_path)
+        
+        grouped = df.groupby(by='image_id')
+        grouped_dict = {image_id: group for image_id, group in grouped}
+        
+        res = defaultdict(list)
+
+        n_id = 0
+        for image_id, (file_name, group) in enumerate(grouped_dict.items()):
+            res['images'].append({
+                'id': image_id,
+                'width': 1024,
+                'height': 1024,
+                'file_name': f'{file_name}.jpg',
+            })
+
+            self.id_csv2coco[file_name] = image_id
+
+            for _, row in group.iterrows():
+                x1, y1, w, h = literal_eval(row['bbox'])
+                res['annotations'].append({
+                    'id': n_id,
+                    'image_id': image_id,
+                    'category_id': 1,
+                    'area': w * h,
+                    'bbox': [x1, y1, w, h],
+                    'iscrowd': 0,
+                })
+                n_id += 1
+
+        res['categories'].extend([{'id': 0, 'name': '_background_'}, {'id': 1, 'name': 'wheat'}])
+            
+        root_dir = os.path.split(csv_path)[0]
+        save_path = os.path.join(root_dir, 'coco_annotations.json')
+        with open(save_path, 'w') as f:
+            json.dump(res, f)
+
+        return save_path
+    
+    def update(self, preds, image_ids):
+        self.preds.extend(list(zip(preds, image_ids)))
+
+    def reset(self):
+        self.preds = []
+
+    def compute(self):
+        detections = []
+        for p, image_id in self.preds:
+            pred_boxes = p['boxes']
+            pred_boxes[:, 2] = pred_boxes[:, 2] - pred_boxes[:, 0]
+            pred_boxes[:, 3] = pred_boxes[:, 3] - pred_boxes[:, 1]
+            pred_boxes = pred_boxes.cpu().numpy()
+
+            pred_scores = p['scores'].cpu().numpy()
+            pred_labels = p['labels'].cpu().numpy()
+
+            image_id = self.id_csv2coco[image_id]
+            for b, s, l in zip(pred_boxes, pred_scores, pred_labels):
+                dt = {
+                    'image_id': image_id,
+                    'category_id': l,
+                    'bbox': b.tolist(),
+                    'score': s
+                }
+                detections.append(dt)
+
+        coco_dt = self.coco_gt.loadRes(detections)
+        coco_eval = COCOeval(self.coco_gt, coco_dt, 'bbox')
+        coco_eval.evaluate()
+        coco_eval.accumulate()
+        coco_eval.summarize()
+
+        coco_map = coco_eval.stats[0]
+
+        return coco_map
+
+
+def test(dataloader: DataLoader, device: _device, model: nn.Module, metric) -> None:
+    """CIFAR-10 데이터셋으로 뉴럴 네트워크의 성능을 테스트합니다.
+
+    :param dataloader: 파이토치 데이터로더
+    :type dataloader: DataLoader
+    :param device: 테스트에 사용되는 장치
+    :type device: _device
+    :param model: 테스트에 사용되는 모델
+    :type model: nn.Module
+    :param loss_fn: 테스트에 사용되는 오차 함수
+    :type loss_fn: nn.Module
+    """
+    num_batches = len(dataloader)
+    test_loss = 0
+    test_cls_loss = 0
+    test_box_loss = 0
+    test_obj_loss = 0
+    test_rpn_loss = 0
+
+    with torch.no_grad():
+        for batch, (images, targets, image_ids) in enumerate(dataloader):
+            images = [image.to(device) for image in images]
+            targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+            model.train()
+            loss_dict = model(images, targets)
+            loss = sum(loss for loss in loss_dict.values())
+
+            test_loss += loss
+            test_cls_loss += loss_dict['loss_classifier']
+            test_box_loss += loss_dict['loss_box_reg']
+            test_obj_loss += loss_dict['loss_objectness']
+            test_rpn_loss += loss_dict['loss_rpn_box_reg']
+
+            model.eval()
+            preds = model(images)
+            # print(preds)
+            metric.update(preds, image_ids)
+
+    test_loss /= num_batches
+    test_cls_loss /= num_batches
+    test_box_loss /= num_batches
+    test_obj_loss /= num_batches
+    test_rpn_loss /= num_batches
+
+    print(f'Test Error: \n Avg loss: {test_loss:>8f} \n Class loss: {test_cls_loss:>8f} \n Box loss: {test_box_loss:>8f} \n Obj loss: {test_obj_loss:>8f} \n RPN loss: {test_rpn_loss:>8f} \n')
+    metric.compute()
+    metric.reset()
+    print()
+
+
+def visualize_predictions(testset: Dataset, device: str, model: nn.Module, save_dir: os.PathLike, conf_thr: float = 0.1, n_images: int = 10) -> None:
     """이미지에 bbox 그려서 저장 및 시각화
     
     :param testset: 추론에 사용되는 데이터셋
@@ -219,14 +385,13 @@ def visualize_predictions(testset: Dataset, device: str, model: nn.Module, save_
         os.makedirs(save_dir)
 
     model.eval()
-    for i, image in enumerate(testset):
+    indices = random.Random(36).choices(range(len(testset)), k=n_images)
+    for i in indices:
+        image, target, image_id = testset[i]
         image = [image.to(device)]
         preds = model(image)
 
         image = image[0].detach().cpu().numpy().transpose(1, 2, 0)
-        # image = image * 255
-
-        image_id = testset.image_ids[i]
 
         plt.imshow(image)
         ax = plt.gca()
@@ -237,7 +402,7 @@ def visualize_predictions(testset: Dataset, device: str, model: nn.Module, save_
                 w = x2 - x1
                 h = y2 - y1
 
-                category_id = 'wheat'
+                category_id = preds[0]['labels']
 
                 rect = patches.Rectangle(
                     (x1, y1),
@@ -269,9 +434,9 @@ def visualize_predictions(testset: Dataset, device: str, model: nn.Module, save_
 
 
 def run_pytorch(
+    csv_path: os.PathLike,
     train_image_dir: os.PathLike,
     train_csv_path: os.PathLike,
-    test_image_dir: os.PathLike,
     test_csv_path: os.PathLike,
     batch_size: int,
     epochs: int,
@@ -284,7 +449,10 @@ def run_pytorch(
     :param epochs: 전체 학습 데이터셋을 훈련하는 횟수
     :type epochs: int
     """
+    split_dataset(csv_path=csv_path)
+    
     visualize_dataset(image_dir=train_image_dir, csv_path=train_csv_path, save_dir='examples/global-wheat-detection/train')
+    visualize_dataset(image_dir=train_image_dir, csv_path=test_csv_path, save_dir='examples/global-wheat-detection/test')
 
     trainset = WheatDataset(
         image_dir=train_image_dir,
@@ -292,65 +460,125 @@ def run_pytorch(
         transform=ToTensor()
     )
     testset = WheatDataset(
-        image_dir=test_image_dir,
+        image_dir=train_image_dir,
         csv_path=test_csv_path,
         transform=ToTensor(),
-        is_test=True
     )
 
     trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=1, collate_fn=collate_fn)
+    testloader = DataLoader(testset, batch_size=batch_size, num_workers=1, collate_fn=collate_fn)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = fasterrcnn_resnet50_fpn(num_classes=1+1).to(device)
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    model = fasterrcnn_resnet50_fpn(num_classes=1+1)
+    model.to(device)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=0.005)
+    metric = MeanAveragePrecision(csv_path=test_csv_path)
 
     for t in range(epochs):
         print(f'Epoch {t+1}\n-------------------------------')
         train(trainloader, device, model, optimizer)
+        print()
+        test(testloader, device, model, metric)
     print('Done!')
 
     torch.save(model.state_dict(), 'wheat-faster-rcnn.pth')
     print('Saved PyTorch Model State to wheat-faster-rcnn.pth')
 
-    model = fasterrcnn_resnet50_fpn(num_classes=1+1).to(device)
+    model = fasterrcnn_resnet50_fpn(num_classes=1+1)
     model.load_state_dict(torch.load('wheat-faster-rcnn.pth'))
-    visualize_predictions(testset, device, model, 'examples/global_wheat_detection/faster-rcnn')
+    model.to(device)
+
+    visualize_predictions(testset, device, model, 'examples/global-wheat-detection/faster-rcnn')
     
     
-def run_pytorch_lightning():
-    class WheatDetectionModule(LightningModule):
-        def __init__(self, num_classes):
-            super().__init__()
-            self.model = fasterrcnn_resnet50_fpn(pretrained=True)
-            in_features = self.model.roi_heads.box_predictor.cls_score.in_features
-            self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-            
-        def forward(self, x):
-            return self.model(x)
+class WheatDetectionModule(LightningModule):
+    def __init__(self, csv_path, lr):
+        super().__init__()
+
+        self.lr = lr
+        self.model = fasterrcnn_resnet50_fpn(num_classes=1+1)
+        self.metric = MeanAveragePrecision(csv_path)
         
-        def training_step(self, batch, batch_idx):
-            images, targets = batch
-            loss_dict = self.model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())
-            return {'loss': losses, 'log': {'train_loss': losses}}
-        
-        def validation_step(self, batch, batch_idx):
-            images, targets = batch
-            loss_dict = self.model(images, targets)
-            losses = sum(loss for loss in loss_dict.values())
-            return {'val_loss': losses}
-        
-        def validation_epoch_end(self, outputs):
-            avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-            return {'avg_val_loss': avg_loss}
-        
-        def configure_optimizers(self):
-            params = [p for p in self.parameters() if p.requires_grad]
-            optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-            scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-            return [optimizer], [scheduler]
-        
-        def train_dataloader(self):
-            return DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
-        
-        def val_dataloader(self):
-            return DataLoader(val_dataset, batch_size=2, shuffle=False, num_workers=4)
+    def forward(self, x):
+        self.model.eval()
+        return self.model(x)
+    
+    def training_step(self, batch, batch_idx):
+        images, targets, _ = batch
+        targets = [{k: v for k, v in t.items()} for t in targets]
+
+        loss_dict = self.model(images, targets)
+        loss = sum(loss for loss in loss_dict.values())
+
+        self.log_dict(loss_dict)
+
+        return {'loss': loss, 'log': loss_dict}
+    
+    def validation_step(self, batch, batch_idx):
+        images, targets, image_ids = batch
+        targets = [{k: v for k, v in t.items()} for t in targets]
+
+        self.model.train()
+        loss_dict = self.model(images, targets)
+        loss = sum(loss for loss in loss_dict.values())
+
+        self.model.eval()
+        preds = self.model(images)
+        self.metric.update(preds, image_ids)
+
+        self.log('val_loss', loss)
+        self.log_dict(loss_dict)
+
+        return {'val_loss': loss, 'log': loss_dict}
+    
+    def validation_epoch_end(self, outputs):
+        self.metric.compute()
+        self.metric.reset()
+    
+    def configure_optimizers(self):
+        return torch.optim.SGD(
+            self.model.parameters(),
+            lr=self.lr,
+            momentum=0.9,
+            weight_decay=0.005,
+        )
+
+
+def run_pytorch_lightning(
+    csv_path: os.PathLike,
+    train_image_dir: os.PathLike,
+    train_csv_path: os.PathLike,
+    test_csv_path: os.PathLike,
+    batch_size: int,
+    epochs: int,
+    lr: float,
+) -> None:
+    split_dataset(csv_path=csv_path)
+    
+    visualize_dataset(image_dir=train_image_dir, csv_path=train_csv_path, save_dir='examples/global-wheat-detection/train')
+    visualize_dataset(image_dir=train_image_dir, csv_path=test_csv_path, save_dir='examples/global-wheat-detection/test')
+
+    trainset = WheatDataset(
+        image_dir=train_image_dir,
+        csv_path=train_csv_path,
+        transform=ToTensor()
+    )
+    testset = WheatDataset(
+        image_dir=train_image_dir,
+        csv_path=test_csv_path,
+        transform=ToTensor(),
+    )
+
+    trainloader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=16, collate_fn=collate_fn)
+    testloader = DataLoader(testset, batch_size=batch_size, num_workers=16, collate_fn=collate_fn)
+
+    model = WheatDetectionModule(csv_path=test_csv_path, lr=lr)
+    wandb_logger = WandbLogger()
+    trainer = Trainer(max_epochs=epochs, accelerator='gpu', devices=1, logger=wandb_logger)
+    trainer.fit(model, train_dataloaders=trainloader, val_dataloaders=testloader)
+
+    trainer.save_checkpoint('wheat-faster-rcnn.ckpt')
+    print('Saved PyTorch Lightning Model State to wheat-faster-rcnn.ckpt')
+
+    model = WheatDetectionModule.load_from_checkpoint(checkpoint_path='wheat-faster-rcnn.ckpt')
+    
+    visualize_predictions(testset, model)
--- test/test_global_wheat_detection.py
+++ test/test_global_wheat_detection.py
@@ -1,25 +1,159 @@
-from hnvlib.global_wheat_detection import visualize_dataset, run_pytorch
+import os
+import json
+import shutil
+import random
+from collections import defaultdict
+from ast import literal_eval
+
+import cv2
+import pandas as pd
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.patheffects as pe
+
+import torch
+
+from hnvlib.global_wheat_detection import split_dataset, visualize_dataset, run_pytorch, run_pytorch_lightning
 
 
-TRAIN_IMAGE_DIR = 'data/global-wheat-detection/train'
-TRAIN_CSV_PATH = 'data/global-wheat-detection/train.csv'
-TEST_IMAGE_DIR = 'data/global-wheat-detection/test'
-TEST_CSV_PATH = 'data/global-wheat-detection/sample_submission.csv'
+CSV_PATH = '../../data/global-wheat-detection/train.csv'
+TRAIN_IMAGE_DIR = '../../data/global-wheat-detection/train'
+TRAIN_CSV_PATH = '../../data/global-wheat-detection/train_answer.csv'
+VAL_CSV_PATH = '../../data/global-wheat-detection/test_answer.csv'
+
+
+def advanced_configure_optimizers(self):
+    params = [p for p in self.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+    return [optimizer], [scheduler]
+
+
+def to_coco(csv_path: os.PathLike) -> os.PathLike:
+    df = pd.read_csv(csv_path)
+    
+    grouped = df.groupby(by='image_id')
+    grouped_dict = {image_id: group for image_id, group in grouped}
+    
+    res = defaultdict(list)
+
+    n_id = 0
+    for image_id, (file_name, group) in enumerate(grouped_dict.items()):
+        res['images'].append({
+            'id': image_id,
+            'width': 1024,
+            'height': 1024,
+            'file_name': f'{file_name}.jpg',
+        })
+
+        for _, row in group.iterrows():
+            x1, y1, w, h = literal_eval(row['bbox'])
+            res['annotations'].append({
+                'id': n_id,
+                'image_id': image_id,
+                'category_id': 1,
+                'area': w * h,
+                'bbox': [x1, y1, w, h],
+                'iscrowd': 0,
+            })
+            n_id += 1
+
+    res['categories'].append([{'id': 0, 'name': '_background_'}, {'id': 1, 'name': 'wheat'}])
+        
+    root_dir = os.path.split(csv_path)[0]
+    save_path = os.path.join(root_dir, 'coco_annotations.json')
+    with open(save_path, 'w') as f:
+        json.dump(res, f)
+
+    return save_path
+
+
+def visualize_coco(json_path: os.PathLike, image_dir: os.PathLike, save_dir: os.PathLike, n_images: int = 10):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    else:
+        shutil.rmtree(save_dir)
+        os.makedirs(save_dir)
+    
+    with open(json_path, 'r') as f:
+        json_file = json.load(f)
+
+    id2fname = {i['id']: i['file_name'] for i in json_file['images']}
+    
+    tmp = defaultdict(list)
+    for ann in tqdm(json_file['annotations']):
+        image_id = ann['image_id']
+        tmp[image_id].append(ann['bbox'])
+
+    for image_id, annots in tqdm(random.Random(36).choices(list(tmp.items()), k=n_images)):
+        file_path = os.path.join(image_dir, id2fname[image_id])
+        
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        plt.imshow(image)
+        ax = plt.gca()
+
+        for x1, y1, w, h in annots:
+            category_id = 'wheat'
+
+            rect = patches.Rectangle(
+                (x1, y1),
+                w, h,
+                linewidth=1,
+                edgecolor='green',
+                facecolor='none'
+            )
+            ax.add_patch(rect)
+            ax.text(
+                x1, y1,
+                category_id,
+                c='white',
+                size=6,
+                path_effects=[pe.withStroke(linewidth=2, foreground='green')],
+                family='sans-serif',
+                weight='semibold',
+                va='top', ha='left',
+                bbox=dict(
+                    boxstyle='round',
+                    ec='green',
+                    fc='green',
+                )
+            )
+
+        plt.axis('off')
+        plt.savefig(os.path.join(save_dir, id2fname[image_id]), dpi=150, bbox_inches='tight', pad_inches=0)
+        plt.clf()
 
 
 def main():
+    # split_dataset(csv_path=CSV_PATH)
     # visualize_dataset(image_dir=TRAIN_IMAGE_DIR, csv_path=TRAIN_CSV_PATH, save_dir='examples/global-wheat-detection/train')
-    run_pytorch(
-        train_image_path=TRAIN_IMAGE_DIR,
+    # visualize_dataset(image_dir=TRAIN_IMAGE_DIR, csv_path=VAL_CSV_PATH, save_dir='examples/global-wheat-detection/test')
+    
+    # json_path = to_coco(VAL_CSV_PATH)
+    # visualize_coco(json_path, TRAIN_IMAGE_DIR, 'examples/global-wheat-detection/coco')
+    
+    # run_pytorch(
+    #     csv_path=CSV_PATH,
+    #     train_image_dir=TRAIN_IMAGE_DIR,
+    #     train_csv_path=TRAIN_CSV_PATH,
+    #     test_csv_path=VAL_CSV_PATH,
+    #     batch_size=16,
+    #     epochs=2,
+    #     lr=1e-3
+    # )
+    run_pytorch_lightning(
+        csv_path=CSV_PATH,
+        train_image_dir=TRAIN_IMAGE_DIR,
         train_csv_path=TRAIN_CSV_PATH,
-        test_image_dir=TEST_IMAGE_DIR,
-        test_csv_path=TEST_CSV_PATH,
-        batch_size=8,
-        epochs=10,
-        lr=5e-3
+        test_csv_path=VAL_CSV_PATH,
+        batch_size=16,
+        epochs=2,
+        lr=1e-3
     )
 
 
 if __name__ == '__main__':
     main()
-    
